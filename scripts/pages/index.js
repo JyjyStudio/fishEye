0977a04d301ec75062726c1a907dd06e1e33c9d5
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-03-31 18:57:11 +0200

    clean code with semistandardJS Linter

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,46 @@
-//récupere les données du json, les fetch, et renvoi la data (photographers) via PhotographersApi
-async function getPhotographersInfos() {
-    const photographersApi =  new PhotographersApi("/Projet-6/data/photographers.json");
-    
-    const photographersInfos = await photographersApi.getPhotographersInfos();
+// récupere les données du json, les fetch, et renvoi la data (photographers) via PhotographersApi
+async function getPhotographersInfos () {
+  const photographersApi = new PhotographersApi('/Projet-6/data/photographers.json');
 
-    return photographersInfos;
+  const photographersInfos = await photographersApi.getPhotographersInfos();
+
+  return photographersInfos;
 }
 
 // pour chaque photographe; on cree une card via createUserCard() de la factory
-async function displayData(photographersInfos) {
-    const photographersSection = document.querySelector(".photographer_section");
-    
-    photographersInfos.forEach(photographer => {
-        const photographerModel = photographerFactory(photographer);
-        // console.log(photographerModel);
-        const userCard = photographerModel.createUserCard();
-        photographersSection.innerHTML += userCard;
-    });
-};
+async function displayData (photographersInfos) {
+  const photographersSection = document.querySelector('.photographer_section');
+
+  photographersInfos.forEach(photographer => {
+    const photographerModel = photographerFactory(photographer);
+    // console.log(photographerModel);
+    const userCard = photographerModel.createUserCard();
+    photographersSection.innerHTML += userCard;
+  });
+}
 
 // récupere les données de l'api, et les affiche
-async function init() {
-    const photographersInfos = await getPhotographersInfos();
-    
-    displayData(photographersInfos);
+async function init () {
+  const photographersInfos = await getPhotographersInfos();
 
-    preventSpace()
-};
+  displayData(photographersInfos);
+
+  preventSpace();
+}
 
 // on execute la fonction init
 init();
 
 // empêche de faire le scroll par default lorsque l'on clique sur la touche
 // espace pour les liens (logo et profils)
-function preventSpace() {
-    const links = document.querySelectorAll('a');
-    links.forEach( profil => {
-        profil.addEventListener('keypress', e => {
-            if(e.keyCode == 32){ //la touche espace renvoi l'utilisateur vers la page demandée
-                e.preventDefault();
-                e.target.click();
-            }
-        });
+function preventSpace () {
+  const links = document.querySelectorAll('a');
+  links.forEach(profil => {
+    profil.addEventListener('keypress', e => {
+      if (e.keyCode === 32) { // la touche espace renvoi l'utilisateur vers la page demandée
+        e.preventDefault();
+        e.target.click();
+      }
     });
+  });
 }
