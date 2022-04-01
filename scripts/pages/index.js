63b6500e6e171a79bd1f0dce45f9e1a166018b60
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-04-01 13:21:54 +0200

    feat : ajout des /*global ...*/ pour eslint

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,31 +1,33 @@
+/*global PhotographersApi, photographerFactory*/
+
 // récupere les données du json, les fetch, et renvoi la data (photographers) via PhotographersApi
 async function getPhotographersInfos () {
-  const photographersApi = new PhotographersApi('./data/photographers.json');
+	const photographersApi = new PhotographersApi('./data/photographers.json');
 
-  const photographersInfos = await photographersApi.getPhotographersInfos();
+	const photographersInfos = await photographersApi.getPhotographersInfos();
 
-  return photographersInfos;
+	return photographersInfos;
 }
 
 // pour chaque photographe; on cree une card via createUserCard() de la factory
 async function displayData (photographersInfos) {
-  const photographersSection = document.querySelector('.photographer_section');
+	const photographersSection = document.querySelector('.photographer_section');
 
-  photographersInfos.forEach(photographer => {
-    const photographerModel = photographerFactory(photographer);
-    // console.log(photographerModel);
-    const userCard = photographerModel.createUserCard();
-    photographersSection.innerHTML += userCard;
-  });
+	photographersInfos.forEach(photographer => {
+		const photographerModel = photographerFactory(photographer);
+		// console.log(photographerModel);
+		const userCard = photographerModel.createUserCard();
+		photographersSection.innerHTML += userCard;
+	});
 }
 
 // récupere les données de l'api, et les affiche
 async function init () {
-  const photographersInfos = await getPhotographersInfos();
+	const photographersInfos = await getPhotographersInfos();
 
-  displayData(photographersInfos);
+	displayData(photographersInfos);
 
-  preventSpace();
+	preventSpace();
 }
 
 // on execute la fonction init
@@ -34,13 +36,13 @@ init();
 // empêche de faire le scroll par default lorsque l'on clique sur la touche
 // espace pour les liens (logo et profils)
 function preventSpace () {
-  const links = document.querySelectorAll('a');
-  links.forEach(profil => {
-    profil.addEventListener('keypress', e => {
-      if (e.keyCode === 32) { // la touche espace renvoi l'utilisateur vers la page demandée
-        e.preventDefault();
-        e.target.click();
-      }
-    });
-  });
+	const links = document.querySelectorAll('a');
+	links.forEach(profil => {
+		profil.addEventListener('keypress', e => {
+			if (e.keyCode === 32) { // la touche espace renvoi l'utilisateur vers la page demandée
+				e.preventDefault();
+				e.target.click();
+			}
+		});
+	});
 }
