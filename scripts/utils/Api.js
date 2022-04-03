78842e00578ea216aa2356e8ca8ecc3320a3f781
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-04-03 14:50:47 +0200

    unused vars fix elslint

--- scripts/utils/Api.js
+++ scripts/utils/Api.js
@@ -1,23 +1,24 @@
+// eslint-disable-next-line no-unused-vars
 class PhotographersApi {
-  /**
+	/**
 	 *
 	 * @param {string} url
 	 */
-  constructor (url) {
-    this.url = url;
-  }
+	constructor (url) {
+		this.url = url;
+	}
 
-  async getPhotographersInfos () {
-    return fetch(this.url)
-      .then(res => res.json())
-      .then(res => res.photographers)
-      .catch(err => console.log('an error occurs', err));
-  }
+	async getPhotographersInfos () {
+		return fetch(this.url)
+			.then(res => res.json())
+			.then(res => res.photographers)
+			.catch(err => console.log('an error occurs', err));
+	}
 
-  async getPhotographersMedias () {
-    return fetch(this.url)
-      .then(res => res.json())
-      .then(res => res.media)
-      .catch(err => console.log('an error occurs', err));
-  }
+	async getPhotographersMedias () {
+		return fetch(this.url)
+			.then(res => res.json())
+			.then(res => res.media)
+			.catch(err => console.log('an error occurs', err));
+	}
 }
