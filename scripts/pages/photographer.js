4b6187fac3fccbb768620e9d785b5e9bf7836f98
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-04-14 00:20:40 +0200

    clean code + fix : index apres tri

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -18,7 +18,7 @@ let allLikes = 0;
 /*****************************************************************************************/
 async function getPhotographersAllInfos () {
 	const photographersApi = new PhotographersApi('./data/photographers.json', 'photographers');
-
+	
 	const photographersInfos = await photographersApi.getPhotographersInfos();
 	const photographersMedias = await photographersApi.getPhotographersMedias();
 	const photographerMedias = photographersMedias.filter(media => {
@@ -123,11 +123,11 @@ async function displayData (photographer) {
 /*****************************************************************************************/
 async function init () {
 
-	const { allInfos, photographerMedias } =  await getPhotographersAllInfos();
-	
+	const { allInfos } =  await getPhotographersAllInfos();
+
 	displayData(allInfos);
 
-	applyFilter(photographerMedias);
+	applyFilter();
 	
 }
 
@@ -226,10 +226,10 @@ async function slider (sens) {
 	if (index > mediaTitles.length - 1) {
 		index = 0;
 	}
-	if (mediaTitles[index].includes('jpg')) {
+	if (photographerMedias[index].image) {
 		hide(lightboxVideo);
 		show(lightboxImage);
-		lightboxImage.src = `./assets/photographers/${id}/${mediaTitles[index]}`;
+		lightboxImage.src = `./assets/photographers/${id}/${photographerMedias[index].image}`;
 		mediaTitle.innerText = photographerMedias[index].title;
 	} else {
 		hide(lightboxImage);
@@ -276,22 +276,29 @@ function preventSpace () {
 //------------------------//------------------------//------------------------//------------------------//
 
 
-function applyFilter(photographerMedias) {
+async function applyFilter() {
+	const { photographerMedias } =  await getPhotographersAllInfos();
 	popularity.addEventListener('click', () => {
 		removeAllChildNodes(content);
 		displayData(photographerMedias.sort(byPopularity));
+		photographerMedias.sort(byPopularity);
+		mediaTitles.sort(byPopularity);
 		displayLightboxImage();
 		displayLightboxVideo();
 	});
 	date.addEventListener('click', () => {
 		removeAllChildNodes(content);
 		displayData(photographerMedias.sort(byDate));
+		photographerMedias.sort(byDate);
+		mediaTitles.sort(byDate);
 		displayLightboxImage();
 		displayLightboxVideo();
 	});
 	titre.addEventListener('click', () => {
 		removeAllChildNodes(content);
 		displayData(photographerMedias.sort(byTitle));
+		photographerMedias.sort(byTitle);
+		mediaTitles.sort(byTitle);
 		displayLightboxImage();
 		displayLightboxVideo();
 	});
